0c6a1202c6b7cd33ccd7950212c0de012503cac3
Steven Senkowski <scsenkowski@gmail.com>
2017-12-01 16:58:28 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,6 +1,12 @@
 require 'nokogiri'
 require 'pry'
 
+# The selector is 'projects: kickstarter.css("li.project.grid_4")'
+# The title is '# title: project.css("h2.bbcard_name strong a").text'
+# The image link is '# image link: project.css("div.project-thumbnail a img").attribute("src").value'
+# The description is 'project.css("p.bbcard_blurb").text'
+# The course location is 'project.css("span.location-name").text'
+# The amount funded is 'project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i'
 def create_project_hash
 
   html = File.read('fixtures/kickstarter.html')
@@ -13,13 +19,6 @@ def create_project_hash
   end
 
   projects
-  # The selector is 'projects: kickstarter.css("li.project.grid_4")'
-  # The title is '# title: project.css("h2.bbcard_name strong a").text'
-  # The image link is '# image link: project.css("div.project-thumbnail a img").attribute("src").value'
-  # The description is 'project.css("p.bbcard_blurb").text'
-  # The course location is 'project.css("span.location-name").text'
-  # The amount funded is 'project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i'
-
 end
 
 create_project_hash
