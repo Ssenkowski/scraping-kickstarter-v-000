3ac2c8e8abc684b9cb0546d5ea35af8469b0f84a
Steven Senkowski <scsenkowski@gmail.com>
2017-12-01 16:56:33 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -12,7 +12,7 @@ def create_project_hash
   # The image link is '# image link: project.css("div.project-thumbnail a img").attribute("src").value'
   # The description is 'project.css("p.bbcard_blurb").text'
   # The course location is 'project.css("span.location-name").text'
-  # The amount funded is 'project.css("ul.project-stats li.first.funded strong").text'
+  # The amount funded is 'project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i'
 
 end
 
